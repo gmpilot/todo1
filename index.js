cc9d3b87cd12c085e33eefe98a3cf35a35ef97ee
Golam Mahadi <61427435+gmpilot@users.noreply.github.com>
2022-02-06 02:13:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const bodyM = document.querySelector("#body1");
+
+
 const divBox = document.querySelector("#div-box");
 const name1 = document.querySelector("#name");
 const time = document.querySelector("#time");
@@ -16,14 +19,16 @@ darkMood.addEventListener("click", function (e) {
   divBox.classList.add("div-box");
   divBox.classList.remove("div-box2");
   console.log("Welcome Dark Mood");
-document.body.style.backroundColor="gray";
+divBox.classList.add("body2");
+  divBox.classList.remove("body1");
 });
 normalMood.addEventListener("click", function (e) {
   e.preventDefault();
   divBox.classList.add("div-box2");
   divBox.classList.remove("div-box");
   console.log("Welcome Normal Mood");
-document.body.style.backroundColor="pink";
+divBox.classList.add("body1");
+  divBox.classList.remove("body2");
 });
 
 // dark mood end
